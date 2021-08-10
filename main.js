feadc92148df619813a4d5036405e9377af821c9
rickyjena <ricky.jena@gmail.com>
2021-08-10 00:12:19 -0700

    Created code for checking validity of blockchain after trying to manipulate its data

--- main.js
+++ main.js
@@ -31,10 +31,36 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash != previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let rickCoin = new Blockchain();
 rickCoin.addBlock(new Block(1, "08/08/2021", { amount: 10}));
-rickCoin.addBlock(new Block(1, "08/07/2021", { amount: 20}));
+rickCoin.addBlock(new Block(2, "08/07/2021", { amount: 20}));
 
 console.log(JSON.stringify(rickCoin, null, 3));
+
+console.log("Is blockchain valid? " + rickCoin.isChainValid());
+
+rickCoin.chain[1].data = {amount:500};
+rickCoin.chain[1].hash = rickCoin.chain[1].calculateHash();
+
+console.log(JSON.stringify(rickCoin, null, 3));
+
+console.log("Is blockchain valid? " + rickCoin.isChainValid());
