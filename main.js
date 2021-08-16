e05e5e8102918f85464cb95ed2f6381c532b7901
rickyjena <ricky.jena@gmail.com>
2021-08-16 16:31:12 -0700

    Implementing and testing proof of work

--- main.js
+++ main.js
@@ -7,15 +7,25 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -28,7 +38,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -51,16 +61,20 @@ class Blockchain{
 }
 
 let rickCoin = new Blockchain();
+
+console.log('Mining Block 1...');
 rickCoin.addBlock(new Block(1, "08/08/2021", { amount: 10}));
+
+console.log('Mining Block 2...');
 rickCoin.addBlock(new Block(2, "08/07/2021", { amount: 20}));
 
-console.log(JSON.stringify(rickCoin, null, 3));
+// console.log(JSON.stringify(rickCoin, null, 3));
 
-console.log("Is blockchain valid? " + rickCoin.isChainValid());
+// console.log("Is blockchain valid? " + rickCoin.isChainValid());
 
-rickCoin.chain[1].data = {amount:500};
-rickCoin.chain[1].hash = rickCoin.chain[1].calculateHash();
+// rickCoin.chain[1].data = {amount:500};
+// rickCoin.chain[1].hash = rickCoin.chain[1].calculateHash();
 
-console.log(JSON.stringify(rickCoin, null, 3));
+// console.log(JSON.stringify(rickCoin, null, 3));
 
-console.log("Is blockchain valid? " + rickCoin.isChainValid());
+// console.log("Is blockchain valid? " + rickCoin.isChainValid());
