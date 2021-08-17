fa52a5bcdcda62f52469f99b212280d9703c7839
rickyjena <ricky.jena@gmail.com>
2021-08-16 18:09:22 -0700

    reorganized files and fixed gitignore file

--- src/blockchain.js
+++ src/blockchain.js
@@ -95,16 +95,5 @@ class Blockchain{
     }
 }
 
-let rickCoin = new Blockchain();
-rickCoin.createTransaction(new Transaction('address1', 'address2', 150));
-rickCoin.createTransaction(new Transaction('address2', 'address1', 45));
-
-console.log('\n Starting miner ...');
-rickCoin.minePendingTransaction('derricks-address');
-
-console.log('\nBalance of Derrick is', rickCoin.getBalanceOfAddress('derricks-address'));
-
-console.log('\n Starting miner again...');
-rickCoin.minePendingTransaction('derricks-address');
-
-console.log('\nBalance of Derrick is', rickCoin.getBalanceOfAddress('derricks-address'));
+module.exports.Blockchain = Blockchain;
+module.exports.Transaction = Transaction;
